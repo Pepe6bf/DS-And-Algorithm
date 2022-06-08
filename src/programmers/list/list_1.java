e8da05005aff14dab6c599ae4de1a86195ecc893
Pepe6bf <pepe6bf@gmail.com>
2022-06-08 19:13:46 +0900

    Programmers: 최댓값 인덱스 구하기 코드 정리

--- src/programmers/list/list_1.java
+++ src/programmers/list/list_1.java
@@ -1,5 +1,6 @@
 package programmers.list;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
@@ -7,18 +8,20 @@ public class list_1 {
     public static void main(String[] args) {
         int[] input = {1, 3, 5, 4, 5, 2, 1};
 
-        Solution1 s1 = new Solution1();
-        int[] answer1 = s1.solution(input);
+        // 첫번째 시도
+        int[] result1 = list_1.solution1(input);
+        System.out.println("result1 = " + Arrays.toString(result1));
 
-        System.out.println(Arrays.toString(answer1));
+        // 두번째 시도
+        int[] result2 = list_1.solution2(input);
+        System.out.println("result2 = " + Arrays.toString(result2));
+
     }
-}
 
-/**
- * 첫 번째 시도 : 일단 답 나오도록 풀이
- */
-class Solution1 {
-    public int[] solution(int[] arr) {
+    /**
+     * 첫 번째 시도 : 일단 답 나오도록 풀이
+     */
+    static int[] solution1(int[] arr) {
         int max = -1;
 
         // 배열의 최대값 추출
@@ -46,13 +49,11 @@ class Solution1 {
 
         return answer;
     }
-}
 
-/**
- * 두 번째 시도 : 람다/스트림 활용
- */
-class Solution2 {
-    public int[] solution(int[] arr) {
+    /**
+     * 두 번째 시도 : 람다/스트림 활용
+     */
+    static int[] solution2(int[] arr) {
         int max = Arrays.stream(arr).max().getAsInt();
         return IntStream.range(0, arr.length)
                 .filter(i -> arr[i] == max)
