aed3ea134ad2e3b6e4b5659e91a9867df978b5ad
Pepe6bf <pepe6bf@gmail.com>
2022-06-11 13:01:04 +0900

    Programmers: 폰켓몬(3-1) 강의 풀이 추가

--- src/programmers/map/Map_1.java
+++ src/programmers/map/Map_1.java
@@ -8,9 +8,13 @@ public class Map_1 {
         int[] input2 = {3,3,3,2,2,4};
         int[] input3 = {3,3,3,2,2,2};
 
-        System.out.println(Map_1.solution1(input1));
-        System.out.println(Map_1.solution1(input2));
-        System.out.println(Map_1.solution1(input3));
+//        System.out.println(Map_1.solution1(input1));
+//        System.out.println(Map_1.solution1(input2));
+//        System.out.println(Map_1.solution1(input3));
+
+        System.out.println(Map_1.solution2(input1));
+        System.out.println(Map_1.solution2(input2));
+        System.out.println(Map_1.solution2(input3));
 
     }
 
@@ -32,5 +36,20 @@ public class Map_1 {
 
         result = collectionBox.size();
         return result;
+    }
+
+    private static int solution2(int[] nums) {
+        // 포켓몬의 종류의 개수를 구한다.
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, 0);
+        }
+        int n1 = map.keySet().size();
+
+        // 배열 크기의 1/2을 구한다.
+        int n2 = nums.length /2 ;
+
+        // 최소값을 선택한다.
+        return Math.min(n1, n2);
     }
 }
