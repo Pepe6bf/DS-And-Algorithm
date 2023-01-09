8741857fe7ad5b440b48c988561ef069a9846e49
pepe6bf <pepe6bf@gmail.com>
2023-01-10 01:24:05 +0900

    BaekJoon: 11720 직접 생각한 코드 추가

--- coding-test/BaekJoon/BJ_11720.java
+++ coding-test/BaekJoon/BJ_11720.java
@@ -1,5 +1,25 @@
 import java.util.*;
 
+// [ 직접 생각한 코드 ]
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String s = sc.next();
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            sum += s.charAt(i) - '0';
+        }
+
+        System.out.println(sum);
+    }
+}
+
+============================================================
+
+// [ 서적 예제 코드 ]
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
