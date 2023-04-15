4179e18feaebad87a0081bdf1ef9a562a79250aa
pepe6bf <pepe6bf@gmail.com>
2023-04-16 00:50:10 +0900

    BOJ: 14888 Java 코드 불필요한 sb 제거

--- coding-test/BaekJoon/BOJ_14888/Main.java
+++ coding-test/BaekJoon/BOJ_14888/Main.java
@@ -1,16 +1,17 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    
-    static StringBuilder sb = new StringBuilder();
-    static int N, max, min;
+
+    static int N;
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
     static int[] nums, operators;
 
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        
+
         N = Integer.parseInt(br.readLine());
 
         nums = new int[N + 1];
@@ -24,20 +25,17 @@ public class Main {
         for (int i = 1; i <= 4; i++) {
             operators[i] = Integer.parseInt(st.nextToken());
         }
-
-        max = Integer.MIN_VALUE;
-        min = Integer.MAX_VALUE;
     }
 
-    static int calculator(int operand1, int operator, int operand2) {
-        if (operator == 1) 
-            return operand1 + operand2;
+    static int calculator(int op1, int operator, int op2) {
+        if (operator == 1)
+            return op1 + op2;
         else if (operator == 2)
-            return operand1 - operand2;
+            return op1 - op2;
         else if (operator == 3)
-            return operand1 * operand2;
+            return op1 * op2;
         else
-            return operand1 / operand2;
+            return op1 / op2;
     }
 
     static void recFunc(int k, int value) {
@@ -51,15 +49,14 @@ public class Main {
 
         // recursion logic
         for (int cand = 1; cand <= 4; cand++) {
-            if (operators[cand] >= 1) {
-                operators[cand]--;
+            if (operators[cand] < 1)
+                continue;
+            
+            operators[cand]--;
 
-                int newValue = calculator(value, cand, nums[k + 1]);
+            recFunc(k + 1, calculator(value, cand, nums[k + 1]));
 
-                recFunc(k + 1, newValue);
-
-                operators[cand]++;
-            }
+            operators[cand]++;
         }
     }
 
@@ -68,8 +65,6 @@ public class Main {
 
         recFunc(1, nums[1]);
 
-        sb.append(max).append('\n').append(min);
-
-        System.out.println(sb);
+        System.out.println(max + "\n" + min);
     }
 }
