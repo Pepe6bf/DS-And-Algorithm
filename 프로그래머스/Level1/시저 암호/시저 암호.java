a44a28353b271817713b4b4251f8b77a643c8c15
edgar6bf <edgar6bf@gmail.com>
2023-10-04 10:38:42 +0900

    프로그래머스: 시저 암호 java 코드

---
+++ 프로그래머스/Level1/시저 암호/시저 암호.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        char[] arr = s.toCharArray();
+        for (char c : arr) {
+            sb.append(push(c, n));
+        }
+        
+        return sb.toString();
+    }
+    
+    private char push(char c, int n) {
+        if (!Character.isAlphabetic(c)) return c;
+        
+        int offset = Character.isUpperCase(c) ? 'A' : 'a';
+        int position = c - offset;
+        position = (position + n) % 26;
+        
+        return (char) (position + offset);
+    }
+}
